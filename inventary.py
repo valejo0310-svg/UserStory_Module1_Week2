9af96f6d4521610185ae72d4894e1efe3437d388
Valery Avila <valejo0310@gmail.com>
2026-03-25 21:32:29 -0500

    Add main menu for sales record inventory system

---
+++ inventary.py
@@ -0,0 +1,33 @@
+print("""
+╔════════════════════════════════════════════════════════════╗
+        WELCOME TO THE SALES RECORD RIWI STORE!!             
+╚════════════════════════════════════════════════════════════╝
+""")
+
+while Menu:
+    try:
+        option = int(input(f"""{"-"*24} MAIN MENU {"-"*25}
+1) 🍎 Enter Product
+2) 🧾 Show inventory
+3) 🔍 Calculate statistics 
+4) 💀 Exit
+{"="*60}
+➤ """))
+
+        if option == 1:
+            add_product()
+
+        elif option == 2:
+            show_inventory()
+        elif option == 3:
+            Calculate_statistics()       
+
+        elif option == 4:
+            print("Exiting...")
+            Menu = 0
+
+        else:
+            print("Option not available.\n")
+
+    except ValueError:
+        print("❌ Invalid number.\n")
